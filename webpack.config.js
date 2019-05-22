a22936a5203ea1fe84939325033d95e3bcdd8cc9
綾村豊 <yutaka.ayamura@beeskynet.com>
2019-05-22 22:40:22 +0900

    Babel7対応

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,29 @@
-const path = require('path');           // インポートはCommonJS形式
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.jsx',             // バンドル対象ソースのエントリポイント
-                                        // (バンドル出力ファイルが複数の場合は複数指定)
-  devtool: 'inline-source-map',         // エラー時にバンドル前のソースの位置を表示
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'               // webpack-dev-serverのコンテンツディレクトリ
+    contentBase: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),   // 出力ディレクトリをきれいにする
-    new HtmlWebpackPlugin({             // index.htmlを配置
-      template: './src/index.html',
+    new CleanWebpackPlugin('dist'),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
       filename: 'index.html'
     })
   ],
   output: {
-    filename: 'bundle.js',                 // バンドル出力ファイル
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
-      {
-        test: /\.jsx$/,   // ローダーを適用するファイルを正規表現で指定
-        use: [
-          'babel-loader'
-        ]
-      }
-    ]
-  }
+      { test: /\.jsx?$/, loader: 'babel-loader' },
+      { test: /\.html?$/, loader: 'html-loader' },
+    ],
+  },
 };
-
